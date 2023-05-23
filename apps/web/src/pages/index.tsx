34916d638ab0f503473f6f5be8e39cc828194c5d
Vishal TK <vishalsanthosh1999@gmail.com>
2023-05-24 01:04:38 +0530

    feat: setup radix ui

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="relative flex h-[100vh] w-full flex-col items-center justify-center text-center">
       GuidePilot
-      <Button />
+      <Button>Button</Button>
     </main>
   )
 }
