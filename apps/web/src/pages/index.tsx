0c66fd6138738983c77688abdf563c6ee82c9b3d
Vishal TK <vishalsanthosh1999@gmail.com>
2023-05-24 00:49:12 +0530

    ✨: first commit

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ui/Button";
+
+export default function Home() {
+  return (
+    <main className="relative flex h-[100vh] w-full flex-col items-center justify-center text-center">
+      GuidePilot
+      <Button />
+    </main>
+  );
+}
